852a7257d0d6cca2ff34907034fb17c9689d5672
Tharun Budidha <tharun.b@fastpix.io>
2026-05-05 11:02:23 +0530

    T0.1: literal version per Moodle M5 (was dynamic date-based)

--- version.php
+++ version.php
@@ -2,7 +2,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_fastpix';
-$plugin->version   = (int)date('Ymd') . '02';
+$plugin->version = 2026050501;
 $plugin->requires  = 2024100100;        // Moodle 4.5 LTS
 $plugin->maturity  = MATURITY_BETA;
 $plugin->release   = '1.0.0-dev';
